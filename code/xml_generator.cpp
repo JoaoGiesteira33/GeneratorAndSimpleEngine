ddd6b92b19998a8059abc1d34171da755c5a3035
JoaoGiesteira33 <joaollgiesteira@gmail.com>
2022-03-05 16:41:12 +0000

    xmlgenerator inicio

---
+++ code/xml_generator.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+int gen_sphere(int radius, int slices, int stacks, std::string filename){
+    return 0;
+}
+int gen_box(int size, int grid, std::string filename){
+    return 0;
+}
+int gen_cone(int radius, int height, int slices, int stacks, std::string filename){
+    return 0;
+}
+int gen_plane(int len, int divisions, std::string filename){
+    return 0;
+}
+
+int check_args(int n, char **args){
+    for(int i = 0; i < n; i++)
+        if(!args[i] || !*args[i]) return 0;
+        // else std::cout<<args[i]<<" ";
+    return 1;
+}
+
+
+int main(int argc, char **argv) {
+    if(!argv[1] || !*argv[1]){
+        std::cout<<"Invalid input";
+        return 1;
+    }
+
+    std::string figure = argv[1];
+
+    if(figure == "sphere" && check_args(4, argv+2))
+        gen_sphere(std::atoi(argv[2]),std::atoi(argv[3]),std::atoi(argv[4]),argv[5]);
+
+    else if (figure == "plane" && check_args(3, argv+2))
+        gen_plane(std::atoi(argv[2]),std::atoi(argv[3]), argv[4]);
+    
+    else if (figure == "cone" && check_args(5, argv+2))
+        gen_cone(std::atoi(argv[2]),std::atoi(argv[3]),std::atoi(argv[4]),std::atoi(argv[5]), argv[6]);
+    
+    else if (figure == "box" && check_args(3, argv+2))
+        gen_box(std::atoi(argv[2]),std::atoi(argv[3]), argv[4]);
+    
+    else {
+        std::cout<<"Invalid Figure";
+        return 1;
+    }
+
+    return 0;
+}
