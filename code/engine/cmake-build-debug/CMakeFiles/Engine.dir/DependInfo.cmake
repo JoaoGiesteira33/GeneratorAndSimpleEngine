d332b8bf131c3dc355985f9d7fd81b78273520c3
pedrommf7 <pedro2mferreira@gmail.com>
2022-03-07 21:43:59 +0000

    cenas

---
+++ code/engine/cmake-build-debug/CMakeFiles/Engine.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/extinto77/Desktop/UNI/3-ano/2-semestre/CG/Trabalho/cg-trabalho-pratico/code/engine/main.cpp" "CMakeFiles/Engine.dir/main.cpp.o" "gcc" "CMakeFiles/Engine.dir/main.cpp.o.d"
+  "/home/extinto77/Desktop/UNI/3-ano/2-semestre/CG/Trabalho/cg-trabalho-pratico/code/engine/src/tinyxml2.cpp" "CMakeFiles/Engine.dir/src/tinyxml2.cpp.o" "gcc" "CMakeFiles/Engine.dir/src/tinyxml2.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
