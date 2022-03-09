458e140217998669d80693634b5ad8f28afba3c7
tortuga <daniel2001faria@gmail.com>
2022-03-09 11:10:52 +0000

    fixed prints

--- code/generator/src/main.cpp
+++ code/generator/src/main.cpp
@@ -67,13 +67,15 @@ Point new_point(float x, float y, float z) {
 void write_point(float x, float y, float z, std::ofstream& file){
     file << std::to_string(x) << " "
          << std::to_string(y) << " "
-         << std::to_string(z) << ";";
+         << std::to_string(z)
+         << std::endl;
 }
 
 void write_point(Point p, std::ofstream& file) {
     file << std::to_string(p->x) << " "
          << std::to_string(p->y) << " "
-         << std::to_string(p->z) << ";";
+         << std::to_string(p->z)
+         << std::endl;
 }
 
 int main(int argc, char **argv) {
