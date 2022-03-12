076fca6ae9bac5c017373cc4d95d6473a8d118b3
tortuga <daniel2001faria@gmail.com>
2022-03-12 16:45:43 +0000

    write point no loinger created new line:gp

--- code/generator/src/main.cpp
+++ code/generator/src/main.cpp
@@ -15,15 +15,13 @@ typedef struct point{
 void write_point(float x, float y, float z, std::ofstream& file){
     file << std::to_string(x) << " "
          << std::to_string(y) << " "
-         << std::to_string(z)
-         << std::endl;
+         << std::to_string(z);
 }
 
 void write_point(Point p, std::ofstream& file) {
     file << std::to_string(p->x) << " "
          << std::to_string(p->y) << " "
-         << std::to_string(p->z)
-         << std::endl;
+         << std::to_string(p->z);
 }
 
 int gen_sphere(char** args){
